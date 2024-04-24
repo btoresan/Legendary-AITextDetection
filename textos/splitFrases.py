67657df0cd1a82969bdba260f81890564977d090
Bernardo <bernardo@bernardos-air.inf.ufrgs.br>
2024-04-24 14:52:52 -0300

    mais textos

--- textos/splitFrases.py
+++ textos/splitFrases.py
@@ -1,5 +1,19 @@
-with open('Limpo0.txt', 'r', encoding='utf-8') as arquivo:
+with open('Limpo2.txt', 'r', encoding='utf-8') as arquivo:
     texto = arquivo.read()
     frases = texto.split('.')
     frases = [frase for frase in frases if frase.strip()]
-    print(len(frases))
+    total = (len(frases))
+
+with open('Limpo1.txt', 'r', encoding='utf-8') as arquivo:
+    texto = arquivo.read()
+    frases = texto.split('.')
+    frases = [frase for frase in frases if frase.strip()]
+    total += (len(frases))
+
+with open('Limpo2.txt', 'r', encoding='utf-8') as arquivo:
+    texto = arquivo.read()
+    frases = texto.split('.')
+    frases = [frase for frase in frases if frase.strip()]
+    total += (len(frases))
+
+print(total)
